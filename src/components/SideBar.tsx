7bd2d0e36cf6fda3f02632bc1b9e6e8e192e807a
Thomas Eusebio Vieira <thomas.eusebio.vieira@egmail.com>
2021-03-20 11:56:43 -0300

    Solução feita sem ajuda

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  setSelectedGenreId: any;
+}) {
+
+  const { genres, selectedGenreId, setSelectedGenreId } = props;
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
